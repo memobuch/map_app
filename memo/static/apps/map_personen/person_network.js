ec8da924e4fe91dcc3a25b0dc942e9f292dd38a1
Leona Elisabeth Münzer <133358816+leonaelisabeth@users.noreply.github.com>
2025-12-04 12:25:42 +0100

    Fix person network asset paths

--- memo/static/apps/map_personen/person_network.js
+++ memo/static/apps/map_personen/person_network.js
@@ -2,7 +2,7 @@
     'use strict';
 
     const CONFIG = {
-        geojsonFile: '/memo/static/apps/map/sample_person_network.json',
+        geojsonFile: '/memo/static/apps/map_personen/sample_person_network.json',
         mapCenter: [47.0707, 15.4395],
         mapZoom: 5,
         minZoom: 3,
